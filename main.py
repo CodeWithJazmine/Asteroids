746bc96ead0a680ac777ca6cda0ae2063cafb8d9
CodeWithJazmine <jazmine.chargualaf@gmail.com>
2025-05-15 20:56:59 -0700

    Implement Player rotation and update logic
    - Add PLAYER_TURN_SPEED constant

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
     player = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
+    dt = 0
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
+        player.update(dt)
+
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip() # Refresh the screen
