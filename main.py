6992568e96637a5831c35ee23e73d8d8c4cb4893
CodeWithJazmine <jazmine.chargualaf@gmail.com>
2025-05-15 19:36:01 -0700

    Initialized GUI with pygame
    - add constants.py for game configuration

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-# this allows us to use code from the
-# open-source pygame library
-# throughout this file
+# This is the main file for the Asteroids game
 import pygame
+from constants import *
 
 def main():
+    # Initialize the pygame library
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill("black") # Fill the screen with black
+        pygame.display.flip() # Refresh the screen
+
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 # This line ensures the main() function is only called when this file is run directly
 # and not when it is imported as a module
